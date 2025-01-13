f59127c0082e651a92515e72507b3e7fbbc0a414
Menbalt516 <159486641+Menbalt516@users.noreply.github.com>
2025-01-12 23:17:03 -0400

    first commit

---
+++ mvcproyecto/mvcproyecto/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace mvcproyecto.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ mvcproyecto/mvcproyecto/data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcproyecto.Models;
+
+namespace mvcproyecto.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Producto> Producto { get; set; }
+
+    }
+
+
+}
